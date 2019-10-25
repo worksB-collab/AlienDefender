07fa3081007d1bb0a2d053272370e57cfb921cd1
b <b>
2019-10-25 09:54:48 +0800

    create a protal to generate aliens "gameLevelSetting()" in AlienController that GameScenes can call it to generate corresponding alienTypes

--- src/Controller/AlienController.java
+++ src/Controller/AlienController.java
@@ -38,31 +38,67 @@ public class AlienController {
         stop = 0;
     }
 
-    private void genAlien() {
-        if ((int) (Math.random() * 100) > 10) {
-            aliens.add(new Alien1(-25f, 50f));
-            Alien.setRoute(route);
-            count++;
+    public LinkedList<Alien> getAliens() {
+        return aliens;
+    }
+
+    private void genAlien(float x, float y, int frequency, int alienNum) {
+        if ((int) (Math.random() * 100) >= frequency / 100) {
+            switch (alienNum) {
+                case 1:
+                    aliens.add(new Alien1(x, y));
+                    break;
+//                case 2:
+//                    aliens.add(new Alien2(x, y));
+//                    break;
+//                case 3:
+//                    aliens.add(new Alien3(x, y));
+//                    break;
+//                case 4:
+//                    aliens.add(new Alien4(x, y));
+//                    break;
+//                case 5:
+//                    aliens.add(new Alien5(x, y));
+//                    break;
+//                case 6:
+//                    aliens.add(new Alien6(x, y));
+//                    break;
+//                case 7:
+//                    aliens.add(new Alien7(x, y));
+//                    break;
+//                case 8:
+//                    aliens.add(new Alien8(x, y));
+//                    break;
+//                case 9:
+//                    aliens.add(new Alien9(x, y));
+//                    break;
+//                case 10:
+//                    aliens.add(new Alien10(x, y));
+//                    break;
+            }
         }
     }
 
-    public LinkedList<Alien> getAliens() {
-        return aliens;
+    private void gameLevelSetting(float x, float y, int frequency, int alienNum, int alienQuantity) {
+        if (moveDelay.update()) {
+            if (count < alienQuantity) {
+                if (genDelay.update()) {
+                    genAlien(x, y, frequency, alienNum);
+                    Alien.setRoute(route);
+                    count++;
+                }
+            }
+        }
     }
 
     public void update() {
         //Aliens update
         if (moveDelay.update()) {
-            if (count < 50) { // first chapter > 50 aliens
-                if (genDelay.update()) {
-                    genAlien();
-                }
-            } 
             for (int i = 0; i < aliens.size(); i++) {
                 Alien a = aliens.get(i);
                 a.update();
                 if (a.getY() >= 24f * Global.MIN_PICTURE_SIZE) {
-                    playerController.setHP(playerController.getHP()-a.getAlienNum()*5);
+                    playerController.setHP(playerController.getHP() - a.getAlienNum() * 5);
                     aliens.remove(i);
                 }
             }
@@ -75,9 +111,9 @@ public class AlienController {
             Alien alien = aliens.get(i);
             if (alien.isDead()) {
                 alien.paintDead(g);
-                if (alien.isDead()){// kill counts 
+                if (alien.isDead()) {// kill counts 
                     scoreController.scoreCount(alien.getAlienNum());
-                    playerController.addScore((long)scoreController.countPerKill(alien.getAlienNum()));
+                    playerController.addScore((long) scoreController.countPerKill(alien.getAlienNum()));
                 }
                 if (aliens.get(i).getDeadDelay() % 6 == 0) {
                     aliens.remove(i);
