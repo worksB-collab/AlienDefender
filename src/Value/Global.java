1ac4832c422cc185b86b7afe1e3a61743c6b62a7
chun-Huang <hgfrttlk@gmail.com>
2019-10-25 16:51:57 +0800

    更新PlayerController money繪製

--- src/Value/Global.java
+++ src/Value/Global.java
@@ -64,7 +64,7 @@ public class Global {
     public static final Font FONT_01 = new Font(Font.DIALOG, Font.PLAIN, 120);
     public static final Font FONT_BUTTON = new Font(Font.DIALOG, Font.PLAIN, 40);
     public static final Font FONT_INPUT = new Font(Font.DIALOG, Font.PLAIN, 30);
-    public static final Font FONT_SCORE = new Font(Font.DIALOG, Font.PLAIN, 60);
+    public static final Font FONT_SCORE = new Font(Font.DIALOG, Font.PLAIN, 40);
     public static final Color DEFAULT_FONT_COLOR = Color.BLACK;
     
     //Key
